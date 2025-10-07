79abd8470680a472fa6d7ce849a1da055ec1c2f8
Carlos Arguello <carguello0210@gmail.com>
2025-10-07 18:29:01 -0500

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# --- Build ---
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /src
+
+COPY PropertyHub.sln ./
+COPY src/Domain/*.csproj src/Domain/
+COPY src/Application/*.csproj src/Application/
+COPY src/Infrastructure/*.csproj src/Infrastructure/
+COPY src/Api/*.csproj src/Api/
+
+RUN dotnet restore PropertyHub.sln
+
+COPY src ./src
+
+RUN dotnet publish src/Api/Api.csproj -c Release -o /app/publish /p:UseAppHost=false
+
+# --- Runtime ---
+FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS final
+WORKDIR /app
+
+EXPOSE 5008
+
+COPY --from=build /app/publish ./
+ENTRYPOINT ["dotnet", "Api.dll"]
+
+
+
+
+
+
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -8,8 +8,13 @@ using MongoDB.Driver.Core.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// app.MapGet("/", () => "Hello World!");
+var allowedOrigins = builder.Configuration.GetSection("AllowedCorsOrigins").Get<string[]>() ?? [""];
 
+builder.Services.AddCors(o => o.AddDefaultPolicy(p => p
+    .WithOrigins(allowedOrigins)
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+));
 
 builder.Services.AddControllers();
 
@@ -24,17 +29,20 @@ builder.Services.AddSingleton<IMongoClient>(_ =>
     {
         cb.Subscribe<CommandStartedEvent>(e =>
         {
-            Console.WriteLine($"[Mongo ⏩ {e.CommandName}]");
+            Console.WriteLine($"[Mongo -> {e.CommandName}]");
             Console.WriteLine(e.Command.ToJson(new JsonWriterSettings { Indent = true }));
         });
     };
 
     return new MongoClient(settings);
 });
+
+var dbName = builder.Configuration["Mongo:Database"];
+
 builder.Services.AddSingleton(sp =>
 {
     var mongoClient = sp.GetRequiredService<IMongoClient>();
-    return mongoClient.GetDatabase("realtyHub");
+    return mongoClient.GetDatabase(dbName);
 });
 
 
@@ -42,8 +50,6 @@ builder.Services.AddSingleton(sp =>
 builder.Services.AddScoped<IPropertyQueries, MongoPropertyQueries>();
 
 var app = builder.Build();
-
+app.UseCors();
 app.MapControllers();
-
-
 app.Run();
